c73c9e56985b43c8aa915470aab5d0a187de076e
Firstname Lastname <tetz2442@gmail.com>
2012-08-19 12:49:53 -0500

    Added indiviual status -fixes #6

--- php/outputgrumbles.php
+++ php/outputgrumbles.php
@@ -2,7 +2,7 @@
 function outputGrumbles($grumble, $comments = false, $loggedin = false) {
 	global $conn;
 	if($grumble) {
-		$sql = "SELECT sg.status_id, sg.status_text, DATE_FORMAT(sg.date_submitted, '%b %e, %Y %l:%i %p') AS date_submitted, " . 
+		$sql = "SELECT sg.status_id, sg.status_text, ug.username, DATE_FORMAT(sg.date_submitted, '%b %e, %Y %l:%i %p') AS date_submitted, " . 
 			"ug.user_id, ug.username, vg.votes_up_count FROM status_grumble AS sg " . 
 			"LEFT OUTER JOIN votes_up_grumble AS vg ON sg.status_id = vg.status_id " .
 			"LEFT OUTER JOIN users_grumble AS ug ON " .
@@ -24,7 +24,7 @@ function outputGrumbles($grumble, $comments = false, $loggedin = false) {
 				echo '<div class="grumble-inner-holder">';
 					echo '<div class="grumble-header">';
 						echo '<a href="/profile/' . $row["username"] . '" rel="' . $row["status_id"] . '" class="username" title="Visit profile"><strong>' . $row["username"] . '</strong></a>';
-						echo '<span class="grumble-time" title="' . $row["date_submitted"] . '">' . time_ago($row["date_submitted"]) . '</span>';
+						echo '<span class="grumble-time" title="' . $row["date_submitted"] . '"><a href="/profile/' . $row["username"] . '/grumble/' . $row["status_id"] . '" class="colored-link-1">' . time_ago($row["date_submitted"]) . '</a></span>';
 					echo '</div>';
 					echo '<div class="grumble-text-holder">';
 						echo '<p class="grumble-text">' . stripslashes($row["status_text"]) . '</p>';
@@ -34,13 +34,16 @@ function outputGrumbles($grumble, $comments = false, $loggedin = false) {
 			echo '<div class="grumble-extras">';
 			echo '<div class="grumble-votes">';
 			if(isset($_SESSION["user_id"])) {
-				if($comments == false) {
 					//get the number of comments from the db
 					$sql = "SELECT COUNT(*) FROM comments_grumble WHERE status_id = " . $grumble;
 					$commentcount = mysql_query($sql, $conn);
 					$row3 = mysql_fetch_array($commentcount);
-					echo '<p class="gif-loader-comments"><img src="/images/ajax-loader.gif" width="16" height="16"/></p><p class="comments-view" rel="' . $row["status_id"] . '" data-html="Comments" data-number="(' . $row3[0] . ')" data-comments="' . $row3[0] . '" title="View/Add comments on this Grumble"><a>Comments</a><span>(' . $row3[0] . ')</span></p>';
-				}
+					if($comments) {
+						echo '<p class="gif-loader-comments"><img src="/images/ajax-loader.gif" width="16" height="16"/></p><p class="comments-view" rel="' . $row["status_id"] . '" data-html="Comments" data-number="(' . $row3[0] . ')" data-comments="' . $row3[0] . '" title="View/Add comments on this Grumble">Comments<span>(' . $row3[0] . ')</span></p>';
+					}
+					else {
+						echo '<p class="gif-loader-comments"><img src="/images/ajax-loader.gif" width="16" height="16"/></p><p class="comments-view" rel="' . $row["status_id"] . '" data-html="Comments" data-number="(' . $row3[0] . ')" data-comments="' . $row3[0] . '" title="View/Add comments on this Grumble"><a>Comments</a><span>(' . $row3[0] . ')</span></p>';
+					}
 			}
 			if((isset($result2) && $row2 = mysql_fetch_array($result2))) {
 				echo '<p class="votes-up" title="You have already voted up">Vote up<span class="votes-up-number">(' . $row["votes_up_count"] . ')</span></p>';
@@ -55,9 +58,11 @@ function outputGrumbles($grumble, $comments = false, $loggedin = false) {
 			echo '</div>';
 			echo '<div class="comments">';
 			if($loggedin && $row3[0] > 2) {
+				if(!$comments) {
 				echo '<div class="view-all-comments" rel="' . $row["status_id"] . '"><p>View All Comments(';
 				echo $row3[0];
 				echo ')</p></div>';
+				}
 			}
 				echo '<div class="quick-comment">';
 					echo '<div class="comment-padding">';
