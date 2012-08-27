3bd8d28b85bdf82c61d0467da373f056742c5cb5
Firstname Lastname <tetz2442@gmail.com>
2012-08-27 07:53:47 -0500

    Fixed votes issue -fixes #17

--- php/votes.php
+++ php/votes.php
@@ -8,15 +8,24 @@ if(isset($_POST["vote_up"]) && $_SERVER['REQUEST_METHOD'] == "POST" && isset($_S
 function catchUpVote($voteid) {
 	global $conn;
 	if(isset($_SESSION["user_id"])) {
-		$sql = "UPDATE votes_up_grumble SET " . 
-			"votes_up_count = votes_up_count + 1" . 
-			" WHERE status_id = " . intval($voteid);
-		mysql_query($sql, $conn) or die("Error: " . mysql_error());
-		
-		$sql = "INSERT INTO user_likes_grumble(user_id, status_id) " . 
-			"VALUES (" . $_SESSION["user_id"] . "," . $voteid . ")";
-		mysql_query($sql, $conn) or die("Error: " . mysql_error());
-		echo 1;
+		$sql = "SELECT user_like_id FROM user_likes_grumble " .
+			"WHERE status_id = " . $voteid . " AND " . 
+			"user_id = " . $_SESSION["user_id"];
+		$result = mysql_query($sql, $conn);
+		if(mysql_num_rows($result) == 0) {
+			$sql = "UPDATE votes_up_grumble SET " . 
+				"votes_up_count = votes_up_count + 1" . 
+				" WHERE status_id = " . $voteid;
+			mysql_query($sql, $conn) or die("Error: " . mysql_error());
+			
+			$sql = "INSERT INTO user_likes_grumble(user_id, status_id) " . 
+				"VALUES (" . $_SESSION["user_id"] . "," . $voteid . ")";
+			mysql_query($sql, $conn) or die("Error: " . mysql_error());
+			echo 1;
+		}
+		else {
+			echo 0;	
+		}
 	}
 	else {
 		echo 0;	
