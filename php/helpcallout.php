23ff560a76222ecf6410499386f65b1ae65e76f9
Jon Tetzlaff <tetz2442@gmail.com>
2012-09-12 14:24:51 -0500

    Small fixes

--- php/helpcallout.php
+++ php/helpcallout.php
@@ -1,4 +1,4 @@
-<div id="help-callout">
+<div id="help-callout" class="rounded-radius-small">
 	<img id="callout-arrow" alt="arrow" src="/images/dropdown-arrow.png" width="15" height="8"/>
     <div class="content-padding"></div>
 </div>
