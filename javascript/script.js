155262143d155c1aaaa9d07538f3e0e14f5d65f7
Jon Tetzlaff <tetz2442@gmail.com>
2012-09-25 22:59:20 -0500

    Toastr addition

--- javascript/script.js
+++ javascript/script.js
@@ -695,12 +695,14 @@ $(document).ready(function() {
 					$(element).find("#gif-loader-comment").hide();
 					$(element).find(".button").removeAttr("disabled");
 					if(result == 0 || result == "") {
-						$("#notification-bar p").html("Something went wrong. Please check your entries.").addClass("error");
-						$("#notification-bar").css("marginLeft",-($("#notification-bar").width() / 2)).fadeIn("fast").delay(2500).fadeOut("slow");
+						//$("#notification-bar p").html("Something went wrong. Please check your entries.").addClass("error");
+						//$("#notification-bar").css("marginLeft",-($("#notification-bar").width() / 2)).fadeIn("fast").delay(2500).fadeOut("slow");
+						toastr.error("Something went wrong. Please check your entries.");
 					}
 					else if(result == 1) {
-						$("#notification-bar p").html("Already submitted.").removeClass("available").addClass("error");
-						$("#notification-bar").css("marginLeft",-($("#notification-bar").width() / 2)).fadeIn("fast").delay(2500).fadeOut("slow");
+						//$("#notification-bar p").html("Already submitted.").removeClass("available").addClass("error");
+						//$("#notification-bar").css("marginLeft",-($("#notification-bar").width() / 2)).fadeIn("fast").delay(2500).fadeOut("slow");
+						toastr.warning("Already submitted.");
 					}
 					else if(result != "") {
 						if($(".comment-holder").length > 0) {
@@ -726,8 +728,8 @@ $(document).ready(function() {
 						$("#quick-compose-textarea").val("").css({"height":"20px","background-color":"#FFF9E8"});
 						$("#grumble-comment div").hide();
 						
-						$("#notification-bar p").html("Comment submitted.").removeClass("error").addClass("available");
-						$("#notification-bar").css("marginLeft",-($("#notification-bar").width() / 2)).fadeIn("fast").delay(2500).fadeOut("slow");
+						//$("#notification-bar p").html("Comment submitted.").removeClass("error").addClass("available");
+						//$("#notification-bar").css("marginLeft",-($("#notification-bar").width() / 2)).fadeIn("fast").delay(2500).fadeOut("slow");
 					}
 			});
 		}
@@ -1032,3 +1034,176 @@ function findLink(text) {
 		return 0;	
 	}
 }
+
+// By: Hans Fjällemark and John Papa
+// https://github.com/CodeSeven/toastr
+// 
+// Modified to support css styling instead of inline styling
+// Inspired by https://github.com/Srirangan/notifer.js/
+;(function(window, $) {
+    window.toastr = (function() {
+        var 
+            defaults = {
+                tapToDismiss: true,
+                toastClass: 'toast',
+                containerId: 'toast-container',
+                debug: false,
+                fadeIn: 300,
+                fadeOut: 1000,
+                extendedTimeOut: 1000,
+                iconClasses: {
+                    error: 'toast-error',
+                    info: 'toast-info',
+                    success: 'toast-success',
+                    warning: 'toast-warning'
+                },
+                iconClass: 'toast-info',
+                positionClass: 'toast-top-right',
+                timeOut: 5000, // Set timeOut to 0 to make it sticky
+                titleClass: 'toast-title',
+                messageClass: 'toast-message'
+            },
+
+
+            error = function(message, title) {
+                return notify({
+                    iconClass: getOptions().iconClasses.error,
+                    message: message,
+                    title: title
+                })
+            },
+
+            getContainer = function(options) {
+                var $container = $('#' + options.containerId)
+
+                if ($container.length)
+                    return $container
+
+                $container = $('<div/>')
+                    .attr('id', options.containerId)
+                    .addClass(options.positionClass)
+
+                $container.appendTo($('body'))
+
+                return $container
+            },
+
+            getOptions = function() {
+                return $.extend({}, defaults, toastr.options)
+            },
+
+            info = function(message, title) {
+                return notify({
+                    iconClass: getOptions().iconClasses.info,
+                    message: message,
+                    title: title
+                })
+            },
+
+            notify = function(map) {
+                var 
+                    options = getOptions(),
+                    iconClass = map.iconClass || options.iconClass,
+                    intervalId = null,
+                    $container = getContainer(options),
+                    $toastElement = $('<div/>'),
+                    $titleElement = $('<div/>'),
+                    $messageElement = $('<div/>'),
+                    response = { options: options, map: map }
+
+                if (map.iconClass) {
+                    $toastElement.addClass(options.toastClass).addClass(iconClass)
+                }
+
+                if (map.title) {
+                    $titleElement.append(map.title).addClass(options.titleClass)
+                    $toastElement.append($titleElement)
+                }
+
+                if (map.message) {
+                    $messageElement.append(map.message).addClass(options.messageClass)
+                    $toastElement.append($messageElement)
+                }
+
+                var fadeAway = function() {
+                    if ($(':focus', $toastElement).length > 0)
+                		return
+                	
+                    var fade = function() {
+                        return $toastElement.fadeOut(options.fadeOut)
+                    }
+
+                    $.when(fade()).done(function() {
+                        if ($toastElement.is(':visible')) {
+                            return
+                        }
+                        $toastElement.remove()
+                        if ($container.children().length === 0)
+                            $container.remove()
+                    })
+                }
+
+                var delayedFadeAway = function() {
+                    if (options.timeOut > 0 || options.extendedTimeOut > 0) {
+                        intervalId = setTimeout(fadeAway, options.extendedTimeOut)
+                    }
+                }
+
+                var stickAround = function() {
+                    clearTimeout(intervalId)
+                    $toastElement.stop(true, true)
+                        .fadeIn(options.fadeIn)
+                }
+
+                $toastElement.hide()
+                $container.prepend($toastElement)
+                $toastElement.fadeIn(options.fadeIn)
+
+                if (options.timeOut > 0) {
+                    intervalId = setTimeout(fadeAway, options.timeOut)
+                }
+
+                $toastElement.hover(stickAround, delayedFadeAway)
+
+                if (!options.onclick && options.tapToDismiss) {
+                    $toastElement.click(fadeAway)
+                }
+
+                if (options.onclick) {
+                    $toastElement.click(function() {
+                        options.onclick() && fadeAway()
+                    })
+                }
+
+                if (options.debug) {
+                    console.log(response)
+                }
+
+                return $toastElement
+            },
+
+            success = function(message, title) {
+                return notify({
+                    iconClass: getOptions().iconClasses.success,
+                    message: message,
+                    title: title
+                })
+            },
+
+            warning = function(message, title) {
+                return notify({
+                    iconClass: getOptions().iconClasses.warning,
+                    message: message,
+                    title: title
+                })
+            }
+
+        return {
+            error: error,
+            info: info,
+            options: {},
+            success: success,
+            warning: warning
+        }
+    })()
+} (window, jQuery));
