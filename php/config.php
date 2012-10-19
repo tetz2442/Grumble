b95024592d4cbf488da05bd841e3e75effe1850d
Jon Tetzlaff <tetz2442@gmail.com>
2012-10-18 22:54:42 -0500

    Added database class and config file

---
+++ php/config.php
@@ -0,0 +1,11 @@
+<?php
+//class for setting constants used throughout site
+//set constants for mysql connection
+define ("SQL_HOST", "localhost");
+define ("SQL_USER", "root");
+define ("SQL_PASS", "");
+define ("SQL_DB", "grumble");
+require_once "conn.php";
+//create new instance of database class
+var db = new DB(SQL_HOST, SQL_USER, SQL_PASS, SQL_PASS);
+?>
